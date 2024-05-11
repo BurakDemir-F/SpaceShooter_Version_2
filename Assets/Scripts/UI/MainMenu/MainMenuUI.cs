1370eb696d5d3c15ea47487aba29d87c3b2b65cd
burakfdemir@gmail.com <burakfdemir@gmail.com>
2024-05-11 21:57:38 +0300

    Main menu ui added.

---
+++ Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI.MainMenu
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button _newGame;
+        [SerializeField] private Button _quit;
+        [SerializeField] private string _gameScene = "GameScene";
+        
+        private void Awake()
+        {
+            _newGame.onClick.AddListener(StartGame);
+            _quit.onClick.AddListener(QuitGame);
+        }
+
+        private void OnDestroy()
+        {
+            _newGame.onClick.RemoveListener(StartGame);
+            _quit.onClick.RemoveListener(QuitGame);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(_gameScene);
+        }
+
+        private void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
